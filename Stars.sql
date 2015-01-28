c3f66392a7d353a0c8e1e654f1472e6eb0ef6847
kgullikson88 <kevin.gullikson@gmail.com>
2015-01-28 11:31:57 -0600

    Updated schema to support hierarchical multiple systems. Hopefully it is good enough...

--- Stars.sql
+++ Stars.sql
@@ -16,7 +16,8 @@ DROP TABLE IF EXISTS "cluster";
 CREATE TABLE "cluster" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "name" TEXT, "age" FLOAT, "age_error" FLOAT, "distance" FLOAT, "distance_error" FLOAT, "RA" FLOAT, "DEC" FLOAT, "reference_id" INTEGER, FOREIGN KEY (reference_id) REFERENCES reference (id));
 
 DROP TABLE IF EXISTS "star";
-CREATE TABLE "star" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE, "name" TEXT , "cluster_id" INTEGER,
+CREATE TABLE "star" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE, "name" TEXT ,
+                     "cluster_id" INTEGER, "component" TEXT,
                      "spectral_type" TEXT, "spectral_type_ref_id" INTEGER, 
                      "temperature" FLOAT, "temperature_error" FLOAT, "temperature_ref_id" INTEGER, 
                      "logg" FLOAT, "logg_error" FLOAT, "logg_ref_id" INTEGER, 
@@ -29,33 +30,38 @@ CREATE TABLE "star" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE, 
                      "Vmag" FLOAT, "Vmag_error" FLOAT, "Vmag_ref_id" INTEGER, 
                      "Kmag" FLOAT, "Kmag_error" FLOAT, "Kmag_ref_id" INTEGER, 
                      "RA" FLOAT, "DEC" FLOAT,
-                    FOREIGN KEY (temperature_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (logg_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (mass_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (metallicity_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (spectral_type_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (radius_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (vsini_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (vsys_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (parallax_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (Vmag_ref_id) REFERENCES reference (id), 
-                    FOREIGN KEY (Kmag_ref_id) REFERENCES reference (id),
-                    FOREIGN KEY (cluster_id) REFERENCES cluster (id));
+                     FOREIGN KEY (temperature_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (logg_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (mass_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (metallicity_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (spectral_type_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (radius_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (vsini_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (vsys_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (parallax_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (Vmag_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (Kmag_ref_id) REFERENCES reference (id),
+                     FOREIGN KEY (cluster_id) REFERENCES cluster (id));
 
 DROP TABLE IF EXISTS "star_system";
 CREATE TABLE "star_system" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE);
 
 DROP TABLE IF EXISTS "star_to_star_system";
-CREATE TABLE "star_to_star_system" ("star1_id" INTEGER NOT NULL, "star2_id" INTEGER NOT NULL, "star_system_id" INTEGER NOT NULL,
-                                    "separation" FLOAT, "separation_error" FLOAT, "separation_ref_id" INTEGER, "period" FLOAT,
-                                    "period_error" FLOAT, "spec_ref_id" INTEGER, "component" TEXT,
-                                    "K1" FLOAT, "K1_error" FLOAT, "K2" FLOAT, "K2_error" FLOAT,
-                                    PRIMARY KEY ("star1_id", "star2_id", "star_system_id")
-                                    FOREIGN KEY (star1_id) REFERENCES star (id),
-                                    FOREIGN KEY (star2_id) REFERENCES star (id),
-                                    FOREIGN KEY (star_system_id) REFERENCES star_system (id),
-                                    FOREIGN KEY (separation_ref_id) REFERENCES reference (id),
-                                    FOREIGN KEY (spec_ref_id) REFERENCES reference (id));
+CREATE TABLE "star_to_star_system" ("star_id" INTEGER NOT NULL, "star_system_id" INTEGER NOT NULL,
+                                    PRIMARY KEY ("star_id", "star_system_id")
+                                    FOREIGN KEY (star_id) REFERENCES star (id),
+                                    FOREIGN KEY (star_system_id) REFERENCES star_system (id));
+
+DROP TABLE IF EXISTS "configuration";
+CREATE TABLE "configuration" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+                              "star_system1_id" INTEGER NOT NULL, "star_system2_id" INTEGER NOT NULL,
+                              "separation" FLOAT, "separation_error" FLOAT, "separation_ref_id" INTEGER,
+                              "period" FLOAT, "period_error" FLOAT, "spec_ref_id" INTEGER,
+                              "K1" FLOAT, "K1_error" FLOAT, "K2" FLOAT, "K2_error" FLOAT,
+                              FOREIGN KEY (star_system1_id) REFERENCES star_system (id),
+                              FOREIGN KEY (star_system2_id) REFERENCES star_system (id),
+                              FOREIGN KEY (separation_ref_id) REFERENCES reference (id),
+                              FOREIGN KEY (spec_ref_id) REFERENCES reference (id));
 
 DROP TABLE IF EXISTS "instrument";
 CREATE TABLE "instrument" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "name" TEXT, "reference_id" INTEGER, FOREIGN KEY (reference_id) REFERENCES reference (id));
